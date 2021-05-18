b1d390f2b4bf6d77ce141c497d9248bb6fc669b6
BekirGeris <48486272+BekirGeris@users.noreply.github.com>
2021-05-18 20:29:15 +0300

    Add files via upload

---
+++ SalesAggregateFunctions.sql
@@ -0,0 +1,9 @@
+--MAÐAZALARIN BÝR TAKIM BÝLGÝLERÝNÝ SUBE ADINA GÖRE GÖRÜNTÜLE
+Select 
+BRANCH,
+sum(LINENET) AS 'Toplam Satýs', COUNT(*) as 'Satir Sayisi', 
+MIN(LINENET) as 'En dusuk satis', MAX(LINENET) as 'En yuksek satis', 
+AVG(LINENET) as 'Ortalama Satis'
+from SALES --WHERE BRANCH = 'iSTANBUL Subesi'
+GROUP BY BRANCH
+
